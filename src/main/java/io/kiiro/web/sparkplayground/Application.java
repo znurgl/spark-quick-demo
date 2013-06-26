12f67ce66d80275928b91c886680eeb71e53d245
Greg Bakos <greg@kiiro.io>
2013-06-26 13:37:02 +0100

    first controllers

---
+++ src/main/java/io/kiiro/web/sparkplayground/Application.java
@@ -0,0 +1,35 @@
+package io.kiiro.web.sparkplayground;
+
+import static spark.Spark.*;
+import spark.*;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		get(new Route("/") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return "Frontpage";
+			}
+		});
+
+		get(new Route("/hello") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return "Hello World!";
+			}
+		});
+
+		// matches "GET /hello/foo" and "GET /hello/bar"
+		// request.params(":name") is 'foo' or 'bar'
+		get(new Route("/hello/:name") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return "Hello: " + request.params(":name");
+			}
+		});
+
+	}
+
+}
